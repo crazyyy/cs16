cc8d400aa1307846b47414cd36ee00c3f86caf20
crazyyy <crazyyy@gmail.com>
2016-01-30 20:08:35 +0200

    rel

--- assets/js/uptop.js
+++ assets/js/uptop.js
@@ -4,19 +4,19 @@ WEB: mvcreative.ru
 */
 
 $(function () {
-	$(window).scroll(function () {
-		if ($(this).scrollTop() >= 50) {
-			$('.upTop').fadeIn(200);
-			$('.upTop').hover(function() {
-				$('.upTopZone').fadeIn(100);
-			}, function() {
-				$('.upTopZone').fadeOut(100);
-			});
-		} else {
-			$('.upTop, .upTopZone').fadeOut(200);
-		}
-	});
-	$('.upTopZone, .upTopButton').click(function () {
-		$('body, html').animate({scrollTop:0}, 1000);
-	});
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {
+      $('.upTop').fadeIn(200);
+      $('.upTop').hover(function() {
+        $('.upTopZone').fadeIn(100);
+      }, function() {
+        $('.upTopZone').fadeOut(100);
+      });
+    } else {
+      $('.upTop, .upTopZone').fadeOut(200);
+    }
+  });
+  $('.upTopZone, .upTopButton').click(function () {
+    $('body, html').animate({scrollTop:0}, 1000);
+  });
 });
